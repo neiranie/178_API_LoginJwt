f9728f3a3467fc8e78ae13c109c8328d86a24a1b
neiranie <email_anda@contoh.com>
2026-07-27 16:04:23 +0700

    update

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const komikController = require('../controller/komikController');
 
+const komikController = require("../controller/komikController");
+const userController = require("../controller/userController");
+const authMiddleware = require("../middleware/authMiddleware");
+
+router.post("/register", userController.register);
+router.post("/login", userController.login);
+
+// Public
 router.get('/komik', komikController.getAllKomik);
 router.get('/komik/:id', komikController.getKomikById);
+
+// Protected
 router.post('/komik', komikController.createKomik);
 router.put('/komik/:id', komikController.updateKomik);
 router.delete('/komik/:id', komikController.deleteKomik);
